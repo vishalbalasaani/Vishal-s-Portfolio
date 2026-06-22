add5468287e404bfeb2c0b880ba7e9ff85089d0b
vishal balasaani <vishalbalasaani04@gmail.com>
2026-06-22 19:03:36 +0530

    Further increase hero image size per user request

--- components/hero.tsx
+++ components/hero.tsx
@@ -37,7 +37,7 @@ export default function Hero() {
           
           {/* Mask applied directly to the image wrapper so the fade tightly follows the image size */}
           <div 
-            className="relative w-full sm:w-[90%] md:w-[85%] lg:w-[75%] xl:w-[70%] aspect-square md:mr-8 lg:mr-12"
+            className="relative w-full md:w-[95%] lg:w-[90%] xl:w-[85%] aspect-square md:mr-4 lg:mr-6"
             style={{
               WebkitMaskImage: "radial-gradient(circle at 50% 50%, black 45%, transparent 75%)",
               maskImage: "radial-gradient(circle at 50% 50%, black 45%, transparent 75%)"
